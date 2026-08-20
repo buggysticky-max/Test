1be6309c587801e0ffa24cce4988a79a5ff890fe
buggysticky-max <buggystick@gmail.com>
2026-08-20 12:30:06 +0800

    Main

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hello world");
+
+
+            Console.WriteLine("Fuck idk");
+        }
+    }
+}
